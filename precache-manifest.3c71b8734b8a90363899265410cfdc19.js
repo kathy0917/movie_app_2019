3381c1e5921c5c0254c9a8152c368395969ebff8
kathy95917 <userid@naver.com>
2019-12-07 02:42:42 +0900

    Updates

---
+++ precache-manifest.3c71b8734b8a90363899265410cfdc19.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcff01efbf395ca1a394437296a0da10",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "12f0a00bb1193e43df6c",
+    "url": "/movie_app_2019/static/css/main.75c32158.chunk.css"
+  },
+  {
+    "revision": "18e17f1c73cef50e947f",
+    "url": "/movie_app_2019/static/js/2.e25caf30.chunk.js"
+  },
+  {
+    "revision": "12f0a00bb1193e43df6c",
+    "url": "/movie_app_2019/static/js/main.4590f7be.chunk.js"
+  },
+  {
+    "revision": "26c5953caa52b1651c1d",
+    "url": "/movie_app_2019/static/js/runtime-main.60e209e0.js"
+  }
+]);
